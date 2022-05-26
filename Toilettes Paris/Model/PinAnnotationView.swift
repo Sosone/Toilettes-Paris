f656cc30ba85a30f1f4383b414f6349e95a09d19
Anthony <anthonylaurent.lr@gmail.com>
2022-05-26 16:47:03 +0200

    Update PinAnnotationView.swift

--- Toilettes Paris/Model/PinAnnotationView.swift
+++ Toilettes Paris/Model/PinAnnotationView.swift
@@ -11,6 +11,7 @@ class PinAnnotationView: MKMarkerAnnotationView {
     override var annotation: MKAnnotation? {
         willSet {
             if let pin = newValue as? Pin {
+                clusteringIdentifier = "Pin"
                 switch pin.pinType {
                 case .pmrTrue:
                     markerTintColor = UIColor.red
@@ -22,8 +23,24 @@ class PinAnnotationView: MKMarkerAnnotationView {
                 canShowCallout = true
                 calloutOffset = CGPoint(x: -5, y: 5)
                 rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+                leftCalloutAccessoryView = setupLeft()
             }
         }
+    }
+    
+    func setupLeft() -> UIButton {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        button.setImage(UIImage(named: "distance"), for: .normal)
+        button.addTarget(self, action: #selector(gps), for: .touchUpInside)
+        return button
+    }
+    
+    @objc func gps() {
+        guard let anno = annotation as? Pin else { return }
+        let placemark = MKPlacemark(coordinate: anno.coordinate)
+        let options = [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving]
+        let map = MKMapItem(placemark: placemark)
+        map.openInMaps(launchOptions: options)
     }
     
 }
