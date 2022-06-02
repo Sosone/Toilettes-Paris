a275a51ef6761a8e109d7efffbc0113cabf73e5c
Anthony <anthonylaurent.lr@gmail.com>
2022-06-02 17:34:22 +0200

    Create Toilet.swift

---
+++ Toilettes Paris/Model/Toilet.swift
@@ -0,0 +1,18 @@
+//
+//  Toilet.swift
+//  Toilettes Paris
+//
+//  Created by Anthony Laurent on 28/05/2022.
+//
+
+import Foundation
+import CoreLocation
+
+struct Toilet {
+    var address: String
+    var hourly: String
+    var accessibility: String
+    var latitude: Double
+    var longitude: Double
+//    var coordinate: CLLocationCoordinate2D
+}
