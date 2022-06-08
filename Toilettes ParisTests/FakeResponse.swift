b075ec251285b1ac9cd39fdbadb4e266541bf5d9
Anthony <anthonylaurent.lr@gmail.com>
2022-06-08 10:24:55 +0200

    Create FakeResponse.swift

---
+++ Toilettes ParisTests/FakeResponse.swift
@@ -0,0 +1,14 @@
+//
+//  FakeResponse.swift
+//  Toilettes ParisTests
+//
+//  Created by Anthony Laurent on 08/06/2022.
+//
+
+import Foundation
+
+struct FakeResponse {
+    var response: HTTPURLResponse?
+    var data: Data?
+    var error: Error?
+}
